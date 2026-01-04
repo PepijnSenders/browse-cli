30e0e5bc9b7ca371b9a72d62639bad96501e7886
PepijnSenders <ps@hellofresh.com>
2026-01-04 08:55:47 +0100

    Update Homebrew formula for v1.0.4 with SHA256 hashes

--- homebrew/browse.rb
+++ homebrew/browse.rb
@@ -1,25 +1,24 @@
 class Browse < Formula
   desc "Scrape any webpage to markdown using your browser session"
   homepage "https://github.com/PepijnSenders/browse-cli"
-  version "1.0.3"
+  version "1.0.4"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/PepijnSenders/browse-cli/releases/download/v#{version}/browse-darwin-arm64.tar.gz"
-      # sha256 will be auto-filled after release
-      sha256 "PLACEHOLDER"
+      sha256 "2a3f5ca48ae3fa46f6dccdfb2ed86963fb64bb4832c04601a703eb560b5c40a5"
     end
     on_intel do
       url "https://github.com/PepijnSenders/browse-cli/releases/download/v#{version}/browse-darwin-x64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "816d8085cda85b1eff457893805a7f8c11f1904bc253ff0bc2462eb04668a843"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/PepijnSenders/browse-cli/releases/download/v#{version}/browse-linux-x64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "61ae6c77066539760987dce372303cfc06b003b7512aeaaca4be219e3ad2d670"
     end
   end
 
